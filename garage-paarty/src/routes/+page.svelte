9209db99a2dd4df373c33b10be9bb83d214e7d76
Scott Schaffter <scott@schaffters.com>
2023-03-18 14:12:36 -0600

    Rebuild website in svelte kit

--- garage-paarty/src/routes/+page.svelte
+++ garage-paarty/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Garage Paarty</h1>
+<marquee><img alt="Garage Party" src="garage_party.png"/></marquee> 
+<p>Welcome to the best party of the year! We're throwing a garage paarty that you won't want to miss. Come hang out with us and enjoy some great music, drinks, and good times.</p>
+<p>Don't forget to invite your friends! The more, the merrier. We'll have plenty of room for everyone. </p>
+<p>When: Saturday, April 1st<br>Where: 1234 Elm Street<br>Time: 7:00 pm - 12:00 am</p>
+<p>RSVP to let us know you're coming <a href="#">here</a>.</p>
