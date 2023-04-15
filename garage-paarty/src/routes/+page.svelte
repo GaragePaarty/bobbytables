4e6b42590a3a6145be3fe177c8299ba76d8a0c47
= <myang83@gmail.com>
2023-04-15 11:23:00 -0700

    add openai apikey to .env to prevent leak to github

--- garage-paarty/src/routes/+page.svelte
+++ garage-paarty/src/routes/+page.svelte
@@ -1,44 +1,49 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import { ChatGPTClient } from './ChatGPTClient';
+	import { onMount } from 'svelte';
+	import { ChatGPTClient } from './ChatGPTClient';
 
-  let prompts = [
-    'What are some fun garage party activities?',
-    'What kind of music is best for a garage party?',
-    'How can I decorate my garage for a party?'
-  ];
+	let prompts = [
+		'What are some fun garage party activities?',
+		'What kind of music is best for a garage party?',
+		'How can I decorate my garage for a party?'
+	];
 
-  let selectedPrompt = '';
-  let response = '';
+	let selectedPrompt = '';
+	let response = '';
 
-  // Generate a random prompt
-  function getRandomPrompt() {
-    const randomIndex = Math.floor(Math.random() * prompts.length);
-    return prompts[randomIndex];
-  }
+	// Generate a random prompt
+	function getRandomPrompt() {
+		const randomIndex = Math.floor(Math.random() * prompts.length);
+		return prompts[randomIndex];
+	}
 
-  // Get ChatGPT response
-  async function getChatGPTResponse(prompt: string) {
-    const chatGPTClient = new ChatGPTClient('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');
-    const result = await chatGPTClient.getCompletion(prompt);
-    return result.text.trim();
-  }
+	// Get ChatGPT response
+	async function getChatGPTResponse(prompt: string) {
+		const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
+		const chatGPTClient = new ChatGPTClient(apiKey);
+		const result = await chatGPTClient.getCompletion(prompt);
+		return result.text.trim();
+	}
 
-  // Fetch the response on page load
-  onMount(async () => {
-    selectedPrompt = getRandomPrompt();
-    response = await getChatGPTResponse(selectedPrompt);
-  });
+	// Fetch the response on page load
+	onMount(async () => {
+		selectedPrompt = getRandomPrompt();
+		response = await getChatGPTResponse(selectedPrompt);
+	});
 </script>
 
 <h2>{selectedPrompt}</h2>
 <p>{response}</p>
 
-
-
 <h1>Garage Paarty</h1>
-<marquee><img alt="Garage Party" src="garage_party.png"/></marquee> 
-<p>Welcome to the best party of the year! We're throwing a garage paarty that you won't want to miss. Come hang out with us and enjoy some great music, drinks, and good times.</p>
-<p>Don't forget to invite your friends! The more, the merrier. We'll have plenty of room for everyone. </p>
-<p>When: Saturday, April 1st<br>Where: 1234 Elm Street<br>Time: 7:00 pm - 12:00 am</p>
+<marquee><img alt="Garage Party" src="garage_party.png" /></marquee>
+<p>
+	Welcome to the best party of the year! We're throwing a garage paarty that you won't want to miss.
+	Come hang out with us and enjoy some great music, drinks, and good times.
+</p>
+<p>
+	Don't forget to invite your friends! The more, the merrier. We'll have plenty of room for
+	everyone.
+</p>
+<p>When: Saturday, April 1st<br />Where: 1234 Elm Street<br />Time: 7:00 pm - 12:00 am</p>
 <p>RSVP to let us know you're coming <a href="#">here</a>.</p>
